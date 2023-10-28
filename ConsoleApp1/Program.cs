7f65140f038d833a862a6cc3f87007ec8c9b1f11
olivinny <vinicius.gtoliv@gmail.com>
2023-10-28 18:26:58 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números a serem somados: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O Resultado da soma é : " + Soma(numero1, numero2)) ;
+            Console.WriteLine("digite enter para finalizar o programa");
+            Console.ReadLine();
+    
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
